528b00e47f0ff61aa85426ddf09f295220efd9ce
Jacob Anderson <jacob.anderson10@gmail.com>
2021-06-02 15:04:26 -0500

    added the option to uplaod text files

--- server/app.py
+++ server/app.py
@@ -1,8 +1,22 @@
 from flask import Flask, flash, render_template, request
 import json
+# import flask WT forms
+from flask_wtf import FlaskForm
+from wtforms import FileField
+
+# import flask uploads
+from flask_uploads import configure_uploads, UploadSet
 # here is where you probably want to import you pyUSB methods
 
 app=Flask(__name__)
+app.config['SECRET_KEY'] = 'thisisthedevelopmentsecretkey'
+app.config['UPLOADS_DEFAULT_DEST'] = 'textFiles/'
+
+textFiles = UploadSet('uploads')
+configure_uploads(app, textFiles)
+
+class MyForm(FlaskForm):
+    textUpload = FileField('text')
 
 @app.route('/')
 def home():
@@ -11,62 +25,105 @@ def home():
 
 @app.route('/addPrefixSuffix',methods=['POST','GET'])
 def addPrefixSuffix():
+    form = MyForm()
     if request.method == "GET":
-        return render_template("addPrefixSuffix.html")
+        
+        # If there a get request, just return the html template
+        return render_template("addPrefixSuffix.html", form=form)
     
     else:
-
-        # response1 is a string (i.e. text) from the first text box
+        # if there's a POST request sending data, then get the data
         prefix = request.form['Prefix']
         suffix = request.form['Suffix']
         original = request.form['Original']
-        splitLinesString = original.splitlines()
-        
         out = []
-        for i in splitLinesString:
-            out.append(prefix+i+suffix)
+
+        if len(form.textUpload.data.filename) > 0:
+            filename = textFiles.save(form.textUpload.data)
+            f = open("./uploads/textFiles/"+filename, "r")
+            splitLinesString = f.readlines()
+            f.close()
+            # loop through each line
+            for i in splitLinesString:
+                # add the prefix and suffix to each line
+                out.append(prefix+i[:-1]+suffix) 
+        else: 
+            splitLinesString = original.splitlines()
+            # loop through each line
+            for i in splitLinesString:
+                # add the prefix and suffix to each line
+                out.append(prefix+i+suffix)
+        
+
+        
     
+        # join each line into one string separated by \n so HTML makes a new line
         outFormmatted = '\n'.join(out)
-        message = 'Your text was successfully formatted'
-        return render_template("addPrefixSuffix.html", message=message, json=outFormmatted, original=original, prefix=prefix, suffix=suffix)
+
+        #outputTextFile = open("MyFile.txt","a")
+        message = 'Prefixes/Suffixes were Successfully Added'
+        # using Jinja return the data to the template
+        return render_template("addPrefixSuffix.html", message=message, json=outFormmatted, original=original, prefix=prefix, suffix=suffix, form=form)
 
 
 @app.route('/removeLineBreaks',methods=['POST','GET'])
 def removeLineBreaks():
+    # If there a get request, just return the html template
     if request.method == "GET":
         return render_template("removeLineBreaks.html")
-    
+
+    # if there's a POST request sending data, then get the data
     else:
         original = request.form['Original']
+
+        # split the original text into an array for each line
         splitLinesString = original.splitlines()
         
+        # create a blank 'out' array
         out = []
+
+        # loop through each line and add it to out
         for i in splitLinesString:
             out.append(i)
     
-        outFormmatted = ''.join(out)
-        message = 'Your text was successfully formatted'
+        # create a single string without any line breaks
+        outFormmatted = ' '.join(out)
+        message = 'Line Breaks were Successfully Removed'
+        # using Jinja return the data to the template
         return render_template("removeLineBreaks.html", message=message, json=outFormmatted, original=original)
 
 @app.route('/removeLineNumbering',methods=['POST','GET'])
 def removeLineNumbering():
+    # If there a get request, just return the html template
     if request.method == "GET":
         return render_template("removeLineNumbering.html")
-    
+
+    # if there's a POST request sending data, then get the data
     else:
         original = request.form['Original']
+        # split the original text into an array for each line
         splitLinesString = original.splitlines()
         
+        # create a blank 'out' array
         out = []
+
+        # loop through each line
         for i in splitLinesString:
+            # split each line based on spaces
             iSplit = i.split()
-            #print(iSplit)
+
+            # concatenate each line, but remove everything before the first space
             iSplitCat = ' '.join(iSplit[1:])
-            print(iSplitCat)
+            # join them back together added a new line at the end
             out.append(iSplitCat+'\n')
     
+        # create a single string without any line breaks
         outFormmatted = ''.join(out)
-        message = 'Your text was successfully formatted'
+
+        
+
+        message = 'Line Numbers were Successfully Removed'
+        # using Jinja return the data to the template
         return render_template("removeLineNumbering.html", message=message, json=outFormmatted, original=original)
 
 
