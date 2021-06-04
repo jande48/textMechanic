8c8c285f3b2dc5bdaf74a5bf44bfe6fc95adf652
Jacob Anderson <jacob.anderson10@gmail.com>
2021-06-04 11:53:42 -0500

    added a load button to load a text file into the original box

--- server/app.py
+++ server/app.py
@@ -53,46 +53,45 @@ def addPrefixSuffix():
 
     # if there's a POST request sending data, then get the data
     else:
-        prefix = request.form['Prefix']
-        suffix = request.form['Suffix']
-        original = request.form['Original']
-        out = []
-
+        
         # if theres an attached file, then open it and read lines
         if len(form.textUpload.data.filename) > 0:
             filename = textFiles.save(form.textUpload.data)
             f = open("./textFiles/uploads/"+filename, "r")
             splitLinesString = f.readlines()
             f.close()
-            # loop through each line
-            for i in splitLinesString:
-                # add the prefix and suffix to each line
-                # for files, don't read the last two character [:-1] because they are /n
-                out.append(prefix+i[:-1]+suffix) 
-        
-        # else use the form text
+            originalFromFile = "".join(splitLinesString)
+            return render_template("addPrefixSuffix.html",original=originalFromFile,form=form)
+
         else: 
+            prefix = request.form['Prefix']
+            suffix = request.form['Suffix']
+            original = request.form['Original']
+            #print(original)
+            out = []
+        
+            # else use the form text
             splitLinesString = original.splitlines()
             # loop through each line
             for i in splitLinesString:
                 # add the prefix and suffix to each line
                 out.append(prefix+i+suffix)
         
-        # join each line into one string separated by \n so HTML makes a new line
-        outFormmatted = '\n'.join(out)
-        
-        # create a random number for a download file
-        randomNum = random.randint(0,999)
-        outputTextFile = open("./textFiles/downloads/"+str(randomNum)+".txt","w")
-        
-        # write the data to this file
-        for i in outFormmatted:
-            outputTextFile.writelines(i)
-        outputTextFile.close()
-        message = 'Prefixes/Suffixes were Successfully Added'
+            # join each line into one string separated by \n so HTML makes a new line
+            outFormmatted = '\n'.join(out)
+            
+            # create a random number for a download file
+            randomNum = random.randint(0,999)
+            outputTextFile = open("./textFiles/downloads/"+str(randomNum)+".txt","w")
+            
+            # write the data to this file
+            for i in outFormmatted:
+                outputTextFile.writelines(i)
+            outputTextFile.close()
+            message = 'Prefixes/Suffixes were Successfully Added'
 
-        # using Jinja return the data to the template
-        return render_template("addPrefixSuffix.html", message=message, json=outFormmatted, original=original, prefix=prefix, suffix=suffix, form=form, randomNum=randomNum)
+            # using Jinja return the data to the template
+            return render_template("addPrefixSuffix.html", message=message, json=outFormmatted, original=original, prefix=prefix, suffix=suffix, form=form, randomNum=randomNum)
 
 
 @app.route('/removeLineBreaks',methods=['POST','GET'])
@@ -104,35 +103,35 @@ def removeLineBreaks():
 
     # if there's a POST request sending data, then get the data
     else:
-        original = request.form['Original']
-        
-        # create a blank 'out' array
-        out = []
         if len(form.textUpload.data.filename) > 0:
             filename = textFiles.save(form.textUpload.data)
             f = open("./textFiles/uploads/"+filename, "r")
             splitLinesString = f.readlines()
             f.close()
-            # loop through each line
-            for i in splitLinesString:
-                # add the prefix and suffix to each line
-                out.append(i[:-1]) 
+            originalFromFile = "".join(splitLinesString)
+            return render_template("removeLineBreaks.html",original=originalFromFile,form=form)
+
+        
+        
         else: 
+            original = request.form['Original']
+            # create a blank 'out' array
+            out = []
             # loop through each line and add it to out
             splitLinesString = original.splitlines()
             for i in splitLinesString:
                 out.append(i)
     
-        # create a single string without any line breaks
-        outFormmatted = ' '.join(out)
-        randomNum = random.randint(0,999)
-        outputTextFile = open("./textFiles/downloads/"+str(randomNum)+".txt","w")
-        for i in outFormmatted:
-            outputTextFile.writelines(i)
-        outputTextFile.close()
-        message = 'Line Breaks were Successfully Removed'
-        # using Jinja return the data to the template
-        return render_template("removeLineBreaks.html", message=message, json=outFormmatted, original=original, form=form, randomNum=randomNum)
+            # create a single string without any line breaks
+            outFormmatted = ' '.join(out)
+            randomNum = random.randint(0,999)
+            outputTextFile = open("./textFiles/downloads/"+str(randomNum)+".txt","w")
+            for i in outFormmatted:
+                outputTextFile.writelines(i)
+            outputTextFile.close()
+            message = 'Line Breaks were Successfully Removed'
+            # using Jinja return the data to the template
+            return render_template("removeLineBreaks.html", message=message, json=outFormmatted, original=original, form=form, randomNum=randomNum)
 
 @app.route('/removeLineNumbering',methods=['POST','GET'])
 def removeLineNumbering():
@@ -143,24 +142,19 @@ def removeLineNumbering():
 
     # if there's a POST request sending data, then get the data
     else:
-        original = request.form['Original']
-        # split the original text into an array for each line
-
-        out = []
-
         if len(form.textUpload.data.filename) > 0:
             filename = textFiles.save(form.textUpload.data)
             f = open("./textFiles/uploads/"+filename, "r")
             splitLinesString = f.readlines()
             f.close()
-            # loop through each line
-            for i in splitLinesString:
-                iSplit = i.split()
-                # concatenate each line, but remove everything before the first space
-                iSplitCat = ' '.join(iSplit[1:])
-                # join them back together added a new line at the end
-                out.append(iSplitCat+'\n')
+            originalFromFile = "".join(splitLinesString)
+            return render_template("removeLineNumbering.html",original=originalFromFile,form=form)
+
         else: 
+            original = request.form['Original']
+            # split the original text into an array for each line
+
+            out = []
             # loop through each line
             splitLinesString = original.splitlines()
             for i in splitLinesString:
@@ -171,18 +165,18 @@ def removeLineNumbering():
                 iSplitCat = ' '.join(iSplit[1:])
                 # join them back together added a new line at the end
                 out.append(iSplitCat+'\n')
-    
-        # create a single string without any line breaks
-        outFormmatted = ''.join(out)
-        randomNum = random.randint(0,999)
-        outputTextFile = open("./textFiles/downloads/"+str(randomNum)+".txt","w")
-        for i in outFormmatted:
-            outputTextFile.writelines(i)
-        outputTextFile.close()
-    
-        message = 'Line Numbers were Successfully Removed'
-        # using Jinja return the data to the template
-        return render_template("removeLineNumbering.html", message=message, json=outFormmatted, original=original, form=form, randomNum=randomNum)
+        
+            # create a single string without any line breaks
+            outFormmatted = ''.join(out)
+            randomNum = random.randint(0,999)
+            outputTextFile = open("./textFiles/downloads/"+str(randomNum)+".txt","w")
+            for i in outFormmatted:
+                outputTextFile.writelines(i)
+            outputTextFile.close()
+        
+            message = 'Line Numbers were Successfully Removed'
+            # using Jinja return the data to the template
+            return render_template("removeLineNumbering.html", message=message, json=outFormmatted, original=original, form=form, randomNum=randomNum)
 
 
 @app.route('/findReplace',methods=['POST','GET'])
@@ -194,24 +188,21 @@ def findReplace():
         return render_template("findReplace.html", form=form)
     
     else:
-        # if there's a POST request sending data, then get the data
-        find = request.form['Find']
-        replace = request.form['Replace']
-        original = request.form['Original']
-        out = []
-
         if len(form.textUpload.data.filename) > 0:
             filename = textFiles.save(form.textUpload.data)
             f = open("./textFiles/uploads/"+filename, "r")
             splitLinesString = f.readlines()
             f.close()
-            # loop through each line
-            for i in splitLinesString:
+            originalFromFile = "".join(splitLinesString)
+            return render_template("findReplace.html",original=originalFromFile,form=form)
+        else:
+        # if there's a POST request sending data, then get the data
+            find = request.form['Find']
+            replace = request.form['Replace']
+            original = request.form['Original']
+            out = []
 
-                # add the prefix and suffix to each line
-                outReplaced = i[:-1].replace(find,replace)
-                out.append(outReplaced)
-        else: 
+            
             splitLinesString = original.splitlines()
             # loop through each line
             for i in splitLinesString:
@@ -219,16 +210,16 @@ def findReplace():
                 outReplaced = i.replace(find,replace)
                 out.append(outReplaced)
 
-        # join each line into one string separated by \n so HTML makes a new line
-        outFormmatted = '\n'.join(out)
-        randomNum = random.randint(0,999)
-        outputTextFile = open("./textFiles/downloads/"+str(randomNum)+".txt","w")
-        for i in outFormmatted:
-            outputTextFile.writelines(i)
-        outputTextFile.close()
-        message = 'Successfully Found and Replaced'
-        # using Jinja return the data to the template
-        return render_template("findReplace.html", message=message, json=outFormmatted, original=original, find=find, replace=replace, form=form, randomNum=randomNum)
+            # join each line into one string separated by \n so HTML makes a new line
+            outFormmatted = '\n'.join(out)
+            randomNum = random.randint(0,999)
+            outputTextFile = open("./textFiles/downloads/"+str(randomNum)+".txt","w")
+            for i in outFormmatted:
+                outputTextFile.writelines(i)
+            outputTextFile.close()
+            message = 'Successfully Found and Replaced'
+            # using Jinja return the data to the template
+            return render_template("findReplace.html", message=message, json=outFormmatted, original=original, find=find, replace=replace, form=form, randomNum=randomNum)
 
 if __name__=="__main__":
     app.run(debug=True, host='0.0.0.0')
